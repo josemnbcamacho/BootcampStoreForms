2151a8cc0c3a92d1a42736e7e2c4e07cb2a39fd9
José Camacho <jose.camacho@xpand-it.com>
2019-05-14 16:56:40 +0100

    Initial check-in of module BootcampStoreForms

---
+++ BootcampStoreForms/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using BootcampStoreForms.Pages;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BootcampStoreForms
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new LoginPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ BootcampStoreForms/Pages/LoginPage.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace BootcampStoreForms.Pages
+{
+    public class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            var label = new Label
+            {
+                Text = "Login",
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                HorizontalTextAlignment = TextAlignment.Center,
+                VerticalTextAlignment = TextAlignment.Center
+            };
+
+            var username = new Entry()
+            {
+
+            };
+
+            var password = new Entry()
+            {
+
+            };
+
+            var loginButton = new Button()
+            {
+
+            };
+
+            var mainStack = new StackLayout
+            {
+                Padding = 30,
+                VerticalOptions = LayoutOptions.Center
+            };
+
+            mainStack.Children.Add(label);
+            mainStack.Children.Add(username);
+            mainStack.Children.Add(password);
+            mainStack.Children.Add(loginButton);
+
+            Content = mainStack;
+        }
+    }
+}
